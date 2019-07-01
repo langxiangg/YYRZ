7438b256cc8e1691e69c6e720730c20920057ac0
浪仙 <296348654@qq.com>
2019-07-01 20:20:59 +0800

    122

--- java/com/langxian/Hello.java
+++ java/com/langxian/Hello.java
@@ -3,6 +3,7 @@ package com.langxian;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 
 }
